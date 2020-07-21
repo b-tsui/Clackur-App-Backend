9ce5cc23da0b175f0195fa01acbe159391b19e95
Brandon Tsui <itstsui@gmail.com>
2020-07-21 17:39:13 -0400

    fix jwt

--- auth.js
+++ auth.js
@@ -1,26 +1,26 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
 // Set up Auth0 configuration
 const authConfig = {
-    domain: "dev-1232de9a.auth0.com",
-    audience: "clackurAuthAPI"
+  domain: "dev-1232de9a.auth0.com",
+  audience: "clackurAuthAPI",
 };
 
 // Define middleware that validates incoming bearer tokens
 // using JWKS from dev-1232de9a.auth0.com
 const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
-    }),
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+  }),
 
-    audience: authConfig.audience,
-    issuer: `https://${authConfig.domain}/`,
-    algorithm: ["RS256"]
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"],
+  algorithms: ["RS256"],
 });
-
 
 module.exports = { checkJwt };
